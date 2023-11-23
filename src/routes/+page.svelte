2b0d8334aa90393afdb76e5cc20cac142a8efd10
Joël Mik <contact@joelmik.nl>
2023-11-23 12:55:00 +0100

    style: install and setup TailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex w-full justify-center items-center flex-col h-[100vh]">
+    <h1 class="text-3xl font-bold mb-5">Legal Annotation Tool</h1>
+    <a class="underline text-blue-800" href="https://gitlab.fdmci.hva.nl/se-specialization-2023-1/projects-io/bamischijf/linkextractor">docs</a>
+</div>
