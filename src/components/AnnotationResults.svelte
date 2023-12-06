fa4caa323c6fa3c2cb54cbc1170adb93cb5c2f14
Nick de Vries <nick.de.vries4@hva.nl>
2023-12-06 10:58:21 +0100

    Removed code not meant for main
    For some reason, with the recent config change, regardless of it not being included in the commit, some WIP code leaked into the main branch; causing some issues, hence removed!

--- src/components/AnnotationResults.svelte
+++ src/components/AnnotationResults.svelte
@@ -1,65 +1,7 @@
-<script>
-    import labelStore from "./LabelStore.svelte";
-
-    let showCreateLabelInput = false;
-    let customLabels = [];
-    let customLabel = '';
-    let customColor = '';
-    let selectedColor = '';
-    let selectedLabel = '';
-
-    const preMadeLabels = [
-        { label: 'Label 1  ', color: 'blue' },
-        { label: 'Label 2  ', color: 'green' },
-        { label: 'Label 3  ', color: 'red' },
-        // Add more pre-made labels with colors as needed
-    ];
-
-    function selectLabel(label, color) {
-        labelStore.update(store => {
-            return { ...store, selectedColor: color, labelID: label};
-        })
-    }
-
-    function createNewLabel() {
-        customLabel = customLabel.trim(); // Remove leading/trailing whitespace
-        if (customLabel !== '' && customColor !== '') {
-            customLabels = [...customLabels, { label: customLabel, color: customColor }];
-            customLabel = '';
-        }
-    }
-
+<script lang="ts">
 
 </script>
 
-<main>
-    <div class="annotation-results h-[80vh] w-[750px] relative mt-5 mb-10">
-        <h1>Label Selection</h1>
-        <h4>Select Label:</h4>
-        <div>
-            {#each preMadeLabels as preMadeLabel}
-                <span style="color: {preMadeLabel.color}" on:click={() => selectLabel(preMadeLabel.label, preMadeLabel.color)}>{preMadeLabel.label}</span>
-            {/each}
-            <span on:click={() => { showCreateLabelInput = true; }}>Create New Label</span>
-        </div>
-        {#each customLabels as labelObj}
-            <div style="color: {labelObj.color}" >{labelObj.label}</div>
-        {/each}
-        {#if showCreateLabelInput}
-            <input type="text" bind:value={customLabel} placeholder="Enter custom label">
-            <input type="color" bind:value={customColor}>
-            <button on:click={createNewLabel}>Create</button>
-        {/if}
-    </div>
-</main>
-
-<style>
-    .annotation-results {
-        font-family: 'Inter', sans-serif;
-        white-space: pre-line;
-        overflow-x: auto;
-        padding: 1em;
-        border-radius: 8px;
-        list-style-type: none;
-    }
-</style>
+<div>
+    <h2 class="text-3xl font-bold mb-5">Annotaties</h2>
+</div>
