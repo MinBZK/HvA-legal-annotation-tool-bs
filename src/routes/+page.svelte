da2c2dd2256357ad8eaa368245f5c1b9111f725f
Nick de Vries <nick.de.vries4@hva.nl>
2023-12-13 19:52:50 +0100

    feat: changed layout & styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,37 @@
 <script>
-    import UploadFile from "../components/UploadFile.svelte";
-    import AnnotationView from "../components/AnnotationView.svelte";
-    import FilterFile from "../components/FilterFile.svelte";
-    import AnnotationResults from "../components/AnnotationResults.svelte";
-    import AnnotationExport from "../components/AnnotationExport.svelte";
+	import UploadFile from '../components/UploadFile.svelte';
+	import AnnotationView from '../components/AnnotationView.svelte';
+	import FilterFile from '../components/FilterFile.svelte';
+	import AnnotationResults from '../components/AnnotationResults.svelte';
+	import AnnotationExport from '../components/AnnotationExport.svelte';
+	import ImportXml from '../components/ImportXML.svelte';
 
-    let fileContent = "";
+	let fileContent = '';
 </script>
 
-<div class="mx-10 mt-10 flex gap-10">
-    <div class="flex flex-col gap-10 border border-gray-200 rounded-lg p-4">
-        <UploadFile on:fileUploaded={e => fileContent = e.detail} />
-        <FilterFile />
-    </div>
-    <div>
-        <AnnotationView fileContent={fileContent} />
-    </div>
-</div>
+{#if !fileContent}
+	<div class="flex flex-col">
+		<div class="self-center">
+			<ImportXml
+				on:fileUploaded={(e) => {
+					fileContent = e.detail;
+					console.log(fileContent);
+				}}
+			/>
+		</div>
+	</div>
+{:else}
+	<div class="flex flex-row">
+		<div
+			class="w-1/2 border dark:border-gray-200 border-gray-950 m-5 p-5 rounded-lg h-[90vh] overflow-auto"
+		>
+			<AnnotationView />
+			<AnnotationExport />
+		</div>
+		<div
+			class="w-1/2 border dark:border-gray-200 border-gray-950 m-5 p-5 rounded-lg h-[90vh] overflow-auto"
+		></div>
+	</div>
+{/if}
+
 <AnnotationResults />
-<AnnotationExport />
