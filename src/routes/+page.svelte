7d809983555d82bca02a895e03940a61e36dd6bc
Nick de Vries <nick.de.vries4@hva.nl>
2023-12-15 10:15:50 +0100

    feat: fixed page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import UploadFile from '../components/UploadFile.svelte';
 	import AnnotationView from '../components/AnnotationView.svelte';
-	import FilterFile from '../components/FilterFile.svelte';
 	import AnnotationResults from '../components/AnnotationResults.svelte';
 	import AnnotationExport from '../components/AnnotationExport.svelte';
 	import ImportXml from '../components/ImportXML.svelte';
@@ -23,13 +21,13 @@
 	<div class="flex flex-row">
 		<div
 			class="w-1/2 border dark:border-gray-200 border-gray-950 m-5 p-5 rounded-lg h-[90vh] overflow-auto"
+		></div>
+		<div
+			class="w-1/2 border dark:border-gray-200 border-gray-950 m-5 p-5 rounded-lg h-[90vh] overflow-auto"
 		>
 			<AnnotationView />
 			<AnnotationExport />
 		</div>
-		<div
-			class="w-1/2 border dark:border-gray-200 border-gray-950 m-5 p-5 rounded-lg h-[90vh] overflow-auto"
-		></div>
 	</div>
 {/if}
 
