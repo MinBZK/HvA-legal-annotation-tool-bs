ef592d014f5c09186d63708b6a6281d8b39f21a1
lamwf <wing.fung.lam@hva.nl>
2023-12-15 18:37:39 +0100

    fix: bugfix the ts-ignore remove the highlight functionality
    Highlight and pop-up of comments in the text is currently not working. Hence its been now remove.
    ts-ignore from previous commits have now been fixed based on the typescrip's lint.

--- src/components/AnnotationComment.svelte
+++ src/components/AnnotationComment.svelte
@@ -1,21 +1,27 @@
-<script >
-    let selectionStart = 0;
-    let selectionEnd = 0;
-    let selection = '';
-    let showPopup = false;
-    let popupX = 0;
-    let popupY = 0;
-    let additionalComment = '';
+<script lang="ts">
+    let selectionStart: number = 0;
+    let selectionEnd: number = 0;
+    let selection: any;
+    let showPopup: boolean = false;
+    let popupX: number = 0;
+    let popupY: number = 0;
+    let additionalComment: string = '';
   
     // New variables to store selection during input focus
-    let inputFocused = false;
-    let savedSelection = null;
-  
-    const comments = [];
-  
-    // @ts-ignore
+    let inputFocused: boolean = false;
+    let savedSelection: any;
+    // TODO able to save the comments into the new data objects.
+    const comments :Comment[] = [];
     let inputElement;
-  
+
+    // Temporary interface will use the new data class
+    interface Comment {
+      start: number,
+      end: number,
+      text: string,
+      additionalComment: string
+    }
+
     const handleSelectionChange = () => {
       if (!inputFocused) {
         // Delay processing the selection to allow it to stabilize
@@ -24,13 +30,11 @@
     };
   
     const processSelection = () => {
-      // @ts-ignore
       selection = document.getSelection();
   
       if (selection.toString().trim() !== '') {
-        // @ts-ignore
         if (selection.rangeCount > 0) {
-          // @ts-ignore
+          // TODO fix the getRangeAt error
           const range = selection.getRangeAt(0);
           selectionStart = range.startOffset;
           selectionEnd = range.endOffset;
@@ -54,7 +58,7 @@
     const saveComment = () => {
       // Save the additional comment and selection information
       console.log("Additional Comment:", additionalComment );
-      const comment = {
+      const comment: Comment  = {
         start: selectionStart,
         end: selectionEnd,
         text: selection.toString(),
@@ -93,37 +97,13 @@
           const selection = document.getSelection();
   
           // Clear the existing range
-          // @ts-ignore
-          selection.removeAllRanges();
+          selection?.removeAllRanges();
   
           // Add the saved selection range
-          // @ts-ignore
-          selection.addRange(savedSelection.getRangeAt(0));
+          selection?.addRange(savedSelection.getRangeAt(0));
         }
       }, 0);
     };
-  
-    // @ts-ignore
-    function highlightMouseEnter(start, end) {
-      // @ts-ignore
-      const highlightedText = document.querySelector(`span[data-text="${text}"]`);
-      if (highlightedText) {
-        // @ts-ignore
-        highlightedText.style.backgroundColor = "highlighted";
-      }
-    }
-  
-  
-    function highlightMouseLeave(event) {
-      const highlightedText = event.target;
-      highlightedText.title = '';
-    }
-  
-    // Helper function to get the comment for a specific text
-    // @ts-ignore
-    function getCommentForText(text) {
-      return comments.find(comment => comment.text === text);
-    }
   </script>
   
   <svelte:document on:selectionchange={handleSelectionChange} />
@@ -138,29 +118,3 @@
       <button type="button" class="btn variant-glass-surface rounded-full" on:click={closePopup}>Close</button>
     </div>
   {/if}
-  
-  <p>
-    {#each comments as { start, end, text, additionalComment }}
-      {#if text}
-        {#if start !== undefined && end !== undefined}
-          <span role="button" tabindex="0" style="background-color: yellow; cursor: pointer;" data-text={text} on:mouseenter={() => highlightMouseEnter(start, end)} on:mouseleave={highlightMouseLeave}>
-            {#if start === 0 && end === 0}
-              {text}
-            {:else}
-              <span style="background-color: highlighted;">
-                {text.slice(0, start)}
-                <span style="background-color: yellow;">{text.slice(start, end)}</span>
-                {text.slice(end)}
-              </span>
-            {/if}
-          </span>
-        {:else}
-          {text}
-        {/if}
-      {/if}
-    {/each}
-  </p>
-  
-  
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-  
