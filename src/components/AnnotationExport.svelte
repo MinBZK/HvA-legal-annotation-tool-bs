8870c7ec522a27e509a2454c1de9101d80022053
Mohammed Hussain Karim Faraj <mohammed.hussain.karim.faraj@hva.nl>
2023-12-21 12:58:06 +0100

    Need to pull main, important changes in main

--- src/components/AnnotationExport.svelte
+++ src/components/AnnotationExport.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 	import objStore from '../stores/ObjStore';
+	import { annotationStore } from '../stores/AnnotationStore';
 	const modalStore = getModalStore();
 
 	let data = $objStore;
 
 	function handleClickExport(fileName = '', data: Object) {
+        // Due to merge request this is not working anymore..
+        // if(!checkRelationships()) { 
+        //     return;
+        // }
 		new Promise<boolean>((resolve) => {
 			const modal: ModalSettings = {
 				type: 'confirm',
@@ -66,6 +71,65 @@
 
 		document.body.removeChild(element);
 	}
+
+    function checkRelationships() {
+        let annotations;
+        annotationStore.subscribe(e => annotations = e);
+
+        let valid = false;
+
+        for(let annotation of annotations) {
+            switch(annotation.label.toLowerCase()) {
+                case 'rechtsbetrekking':
+                    // Check if there is a relationship of 'wie heeft het recht' that has a target of label 'rechtssubject'
+                    if(!annotation.relationships.some(r => r.type === 'wie heeft het recht' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'rechtssubject')) {
+                        alert(`${annotation.text} misses a relationship of type "wie heeft het recht" with a target of label "rechtssubject"`);
+                        return;
+                    }
+                    // Check the same thing but with 'wie heeft de plicht'
+                    if(!annotation.relationships.some(r => r.type === 'wie heeft de plicht' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'rechtssubject')) {
+                        alert(`${annotation.text} misses a relationship of type "wie heeft de plicht" with a target of label "rechtssubject"`);
+                        return;
+                    }
+                    // Check if there is a relationship of 'heeft als voorwerp' that has a target of label 'rechtsobject'
+                    if(!annotation.relationships.some(r => r.type === 'heeft als voorwerp' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'rechtsobject')) {
+                        alert(`${annotation.text} misses a relationship of type "heeft als voorwerp" with a target of label "rechtsobject"`);
+                        return;
+                    }
+
+                    valid = true;
+                    return;
+                case 'rechtsfeit':
+                    // Check if there is a relationship of 'heeft als voorwerp' that has a target of label 'rechtsobject'
+                    if(!annotation.relationships.some(r => r.type === 'heeft als voorwerp' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'rechtsobject')) {
+                        alert(`${annotation.text} misses a relationship of type "heeft als voorwerp" with a target of label "rechtsobject"`);
+                        return;
+                    }
+                    // Check if there is a relationship of 'vind plaats op' that has a target of label 'tijdsaanduiding'
+                    if(!annotation.relationships.some(r => r.type === 'vind plaats op' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'tijdsaanduiding')) {
+                        alert(`${annotation.text} misses a relationship of type "vind plaats op" with a target of label "tijdsaanduiding"`);
+                        return;
+                    }
+
+                    valid = true;
+                    return;
+                case 'afleidingsregel':
+                    // Check if there is a relationship of 'heeft als uitvoer' that has a target of label 'variabele'
+                    if(!annotation.relationships.some(r => r.type === 'heeft als uitvoer' && annotations.find(a => a.id === r.target).label.toLowerCase() === 'variabele')) {
+                        alert(`${annotation.text} misses a relationship of type "heeft als uitvoer" with a target of label "variabele"`);
+                        return;
+                    }
+
+                    valid = true;
+                    return;
+                default:
+                    valid = true;
+                    return;
+            }
+        }
+
+        return false;
+    }
 </script>
 
 <button
