3caea88df8cb88324ab2a9b2358beb06c147c892
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-06 20:35:05 +0100

    feat: added .env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_URL, PUBLIC_API_KEY } from '$env/static/public';
 
 const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_KEY
+    PUBLIC_URL,
+    PUBLIC_API_KEY
 )
 
 export default supabase
