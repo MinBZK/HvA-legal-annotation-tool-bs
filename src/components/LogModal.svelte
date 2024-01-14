b7ddd2059d67ffcb272d85b671d1de158703ef76
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-14 18:34:15 +0100

    feat: added mock audit log

---
+++ src/components/LogModal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import {
+		Table,
+		getModalStore,
+		type TableSource,
+		tableMapperValues
+	} from '@skeletonlabs/skeleton';
+
+	// local
+	const modalStore = getModalStore();
+
+	const auditLog = [
+		{ id: 1, name: 'Bill', date: '01-01-2001' },
+		{ id: 2, name: 'John', date: '03-03-2003' },
+		{ id: 3, name: 'James', date: '05-05-2005' }
+	];
+
+	const tableLog: TableSource = {
+		head: ['Name', 'Date'],
+		body: tableMapperValues(auditLog, ['name', 'date'])
+	};
+</script>
+
+<!-- @component component for displaying the audit log. -->
+
+{#if $modalStore[0]}
+	<div class="w-1/2">
+        <h2 class="h2 font-mono">Audit Log</h2>
+		<Table source={tableLog} interactive={true} />
+	</div>
+{/if}
