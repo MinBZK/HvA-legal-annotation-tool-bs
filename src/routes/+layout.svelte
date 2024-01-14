99ad4f2dd03e5fbf45cabf375f5f683152912055
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-14 11:19:14 +0100

    feat: +layout overhaul to accomodate audit log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,11 +12,16 @@
 	<Modal />
 	<svelte:fragment slot="header">
 		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-            <svelte:fragment slot="lead">PROTOTYPE BUILD</svelte:fragment>
-			Legal Annotation Tool
-            <svelte:fragment slot="trail">
-                <LightSwitch />
-            </svelte:fragment>
+			<svelte:fragment slot="lead">buttter</svelte:fragment>
+			<h1 class="h3">
+				<span
+					class="bg-gradient-to-br from-red-500 to-yellow-300 bg-clip-text text-transparent box-decoration-clone"
+					>LEGAL ANNOTATION TOOL</span
+				>
+			</h1>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<slot />
