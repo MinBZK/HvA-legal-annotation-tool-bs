b2f3a79d676d85a233f04b1cc890ac32d76791cb
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-14 18:34:15 +0100

    feat: added mock audit log

---
+++ src/components/AuditLog.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import LogModal from '../components/LogModal.svelte';
+	import {
+		getModalStore,
+		initializeStores,
+		Modal,
+		type ModalComponent,
+		type ModalSettings
+	} from '@skeletonlabs/skeleton';
+
+	initializeStores();
+
+	const modalStore = getModalStore();
+
+	const modalComponent: ModalComponent = { ref: LogModal };
+
+	const modal: ModalSettings = {
+		type: 'component',
+		component: modalComponent
+	};
+
+	function handleClick(): void {
+		modalStore.trigger(modal);
+	}
+</script>
+
+<Modal />
+<button
+	class="btn btn-lg variant-filled-tertiary rounded-md"
+	on:click={() => handleClick()}
+	type="button"
+>
+	<Fa size="1.5x" icon={faClockRotateLeft} />
+</button>
