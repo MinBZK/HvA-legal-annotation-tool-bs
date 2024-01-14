8a0417043629b5414745851392bda44277f8dc44
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-15 00:33:46 +0100

    feat: added History property

--- src/models/LegalDocument.ts
+++ src/models/LegalDocument.ts
@@ -1,17 +1,24 @@
 import type Annotation from "./Annotation";
 
+interface History {
+    user: string;
+    date: Date;
+}
+
 export default class LegalDocument {
     title: string;
     filename: string;
     chapterTitles: string[];
     chapterContents: string[];
     annotations: Annotation[];
+    history: History[];
 
-    constructor(title: string, filename: string, chapterTitles: string[],chapterContents: string[], annotations: Annotation[]) {
+    constructor(title: string, filename: string, chapterTitles: string[], chapterContents: string[], annotations: Annotation[], history: History[] = []) {
         this.title = title;
         this.filename = filename;
         this.chapterTitles = chapterTitles;
         this.chapterContents = chapterContents;
         this.annotations = annotations;
+        this.history = history;
     }
 }
