5a50eab67c84ed25bbeb07f75a5616bffc1a1bd9
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-14 18:34:59 +0100

    feat: changed button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,25 @@
 	import { labelStore } from '../stores/LabelStore';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
-	import { Drawer, Modal, Toast, getDrawerStore, initializeStores } from '@skeletonlabs/skeleton';
+	import {
+		Drawer,
+		Modal,
+		Toast,
+		getDrawerStore,
+		initializeStores,
+		type PopupSettings
+	} from '@skeletonlabs/skeleton';
+	import { faPlus } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import documentStore from '../stores/DocumentStore';
+	import type LegalDocument from '../models/LegalDocument';
+	import AuditLog from '../components/AuditLog.svelte';
 
 	let showAnnotations = false;
 	initializeStores();
 	const drawerStore = getDrawerStore();
 
-	let fileContent = '';
+	let fileContent: LegalDocument | null = null;
 
 	onMount(async () => {
 		labelStore.set([]);
@@ -27,7 +39,21 @@
 		} else {
 			labelStore.set(data);
 		}
+
+		documentStore.subscribe((value) => {
+			console.dir(value);
+			if (value.title = '') {
+				fileContent = value;
+				localStorage.setItem('legal-document', JSON.stringify(value));
+			}
+		});
 	});
+
+	const popupHover: PopupSettings = {
+		event: 'hover',
+		target: 'popupHover',
+		placement: 'top'
+	};
 </script>
 
 {#if !fileContent}
@@ -53,8 +79,16 @@
 			<FilterFile />
 		</div>
 		<div class="w-3/4 overflow-auto h-[100vh]">
-			<AnnotationView />
-			<AnnotationExport />
+			<div class="h-1/3">
+				<AnnotationView />
+			</div>
+			<div class="ml-10 absolute bottom-0 right-0 m-10">
+				<btn class="btn btn-lg variant-filled-success rounded-md">
+					<Fa size="1.5x" icon={faPlus} />
+				</btn>
+				<AuditLog />
+				<AnnotationExport />
+			</div>
 		</div>
 		<div class="max-w-48">
 			<LabelInputChips />
