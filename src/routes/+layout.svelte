d2a1c10d0e49a7e26a493a650a10bafab49ae389
Nick de Vries <nick.de.vries4@hva.nl>
2024-01-15 16:03:15 +0100

    feat: start on theme button, changing to pop-up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { AppBar, AppShell, Modal, LightSwitch, initializeStores } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, Modal, LightSwitch, initializeStores, popup } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	import type { ModalComponent } from '@skeletonlabs/skeleton';
+	import type { ModalComponent, PopupSettings } from '@skeletonlabs/skeleton';
 	import LogModal from '../components/LogModal.svelte';
 
 	initializeStores();
@@ -10,6 +10,22 @@
 
 	const modalRegistry: Record<string, ModalComponent> = {
 		modalComponentOne: { ref: LogModal }
+	};
+
+	let theme = 'skeleton';
+
+	function changeTheme(event: any) {
+		theme = event.target.value;
+		document.body.setAttribute('data-theme', theme);
+	};
+
+	const popupStyle: PopupSettings = {
+		event: 'click',
+		target: 'popupStyle',
+		placement: 'bottom',
+		middleware: {
+			shift: 24,
+		}
 	};
 
 	import '../app.css';
@@ -33,7 +49,40 @@
 			</h3>
 			{/if}
 			<svelte:fragment slot="trail">
-				<LightSwitch />
+				<button class = "btn variant-filled" use:popup={popupStyle}></button>
+				<div class ="card p-4 w-72 shadow-xl right-5" data-popup="popupStyle">
+					<div><p>testings!</p></div>
+					<div class="arrow bg-surface-100-800-token"/>
+				</div>
+
+
+
+
+
+				<!-- <div class="flex content-center justify-between">
+					<div class="flex-initial m-1">
+						<select
+							id="theme-selector"
+							class="select size-fit"
+							bind:value={theme}
+							on:change={(e) => {
+								changeTheme(e);
+							}}
+						>
+							<option value="skeleton">Skeleton</option>
+							<option value="wintry">Wintry</option>
+							<option value="rocket">Seafoam</option>
+							<option value="vintage">Vintage</option>
+							<option value="sahara">Sahara</option>
+							<option value="hamlindigo">Hamlindigo</option>
+							<option value="gold-nouveau">Gold Nouveau</option>
+							<option value="crimson">Crimson</option>
+						</select>
+					</div>
+					<div class="flex-initial m-1">
+						<LightSwitch class="size-fit" />
+					</div>
+				</div> -->
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
