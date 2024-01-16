50e8c4381442f4e707e6e43c3ce22c832dbb36dc
Mohammed Hussain Karim Faraj <mohammed.hussain.karim.faraj@hva.nl>
2024-01-16 12:35:52 +0100

    Add input chips for labels

--- src/components/EditAnnotation.svelte
+++ src/components/EditAnnotation.svelte
@@ -1,18 +1,56 @@
 <script lang="ts">
     // TODO: Create adding labels functionality, create removing labels functionality
 	import { annotationStore } from "../stores/AnnotationStore";
-    import { getToastStore } from "@skeletonlabs/skeleton";
+    import { Autocomplete, InputChip, getToastStore, type AutocompleteOption } from "@skeletonlabs/skeleton";
+	import { labelStore } from "../stores/LabelStore";
+	import type Label from "../models/Label";
+	import { onMount } from "svelte";
 
     export let hideComponent: Function;
     export let selectedAnnotationId: number;
 
     const toastStore = getToastStore();
 
+    type completeOptions = AutocompleteOption<string>;
+
     let selectedAnnotation;
+    let labelList: Label[] = [];
+    let selectedLabelList: string[] = [];
+    let autoCompleteOptions: completeOptions[] = [];
+    let chip = '';
+
+    $: {
+        labelStore.subscribe(e => {
+            labelList = e;
+            selectedLabelList = selectedAnnotation.label.map(l => l.name);
+        });
+
+        autoCompleteOptions = labelList.map((label) => ({ label: label.name, value: label.name })).sort((a, b) => a.label.localeCompare(b.label)).filter((label) => !selectedLabelList.includes(label.label));
+    }
 
     annotationStore.subscribe(e => {
         selectedAnnotation = e.find(a => a.id === selectedAnnotationId);
     });
+
+    function onAddLabel(e: CustomEvent) {
+        const labelName = e.detail.value;
+
+        if (labelName) {
+            const labelObject = labelList.find(l => l.name.toLowerCase() === labelName.toLowerCase());
+            if(labelObject === undefined) return;
+            selectedAnnotation.label = [...selectedAnnotation.label, labelObject];
+            selectedLabelList = [...selectedLabelList, labelName];
+        }
+    }
+
+    function onRemoveLabel(e: CustomEvent) {
+        const labelName = e.detail.chipValue;
+
+        if (labelName) {
+            selectedLabelList = selectedLabelList.filter(l => l !== labelName);
+            selectedAnnotation.label = selectedAnnotation.label.filter(l => l.name !== labelName);
+        }
+    }
 
     function submitAnnotation() {
         if (selectedAnnotation.label.length === 0) {
@@ -35,54 +73,40 @@
 </script>
 
 <div>
+    <h1 class="h5 font-bold mb-5 inline">Annotatie: "{selectedAnnotation.text}"</h1>
+
     <button type="button" class="btn variant-filled hover:variant-filled-secondary float-right font-bold mb-3"
-        on:click={hideComponent()}>
-            Terugkeren
-    </button>
-    
-    <h1 class="h5 font-bold mb-5">Annotatie: "{selectedAnnotation.text}"</h1>
-
-    <div class="mb-3 mt-3 w-1/2">
-        <label for="labelName" class="font-bold">Comment</label>
-        <input type="text" class="input" bind:value={selectedAnnotation.comment.comment} />
-    </div>
-
-    <div class="mb-3 mt-3 w-1/2">
-        <label for="labelName" class="font-bold">Definition</label>
-        <input type="text" class="input" bind:value={selectedAnnotation.definition.comment} />
-    </div>
-
-    <div class="table-container mt-10">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Label Naam</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each selectedAnnotation.label as label}
-                    <tr>
-                        <th style="color: {label.color};">{label.name}</th>
-                        <th
-                            ><button
-                                type="button"
-                                class="btn variant-filled-error hover:variant-filled-error-secondary text-white font-bold my-2"
-                                on:click={() => null}>Wissen</button
-                            ></th
-                        >
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-    </div>
-
-    <div class="float-right mt-5">
-        <button type="button" class="btn variant-filled hover:variant-filled-secondary font-bold" on:click={() => null}>
-            Labels toevoegen
+            on:click={() => submitAnnotation()}>
+                Terugkeren
         </button>
-        <button type="button" class="btn variant-filled hover:variant-filled-secondary font-bold" on:click={() => submitAnnotation()}>
-            Opslaan
-        </button>
-    </div>
+
+        <div class="mb-3 mt-3 w-1/2">
+            <label for="labelName" class="font-bold">Comment</label>
+            <input type="text" class="input" bind:value={selectedAnnotation.comment.comment} />
+        </div>
+
+        <div class="mb-3 mt-3 w-1/2">
+            <label for="labelName" class="font-bold">Definition</label>
+            <input type="text" class="input" bind:value={selectedAnnotation.definition.comment} />
+        </div>
+
+        <div>
+            <label for="labelInput" class="font-bold">Labels</label>
+            <InputChip
+                name="labelInput"
+                options={labelList}
+                bind:input={chip}
+                bind:value={selectedLabelList}
+                chips="variant-filled-secondary"
+                allowUpperCase
+                on:remove={e => onRemoveLabel(e)}
+            />
+            <div class="card max-h-52 p-4 overflow-y-auto" tabindex="-1">
+                <Autocomplete
+                    bind:input={chip}
+                    options={autoCompleteOptions}
+                    on:selection={e => onAddLabel(e)}
+                />
+            </div>
+        </div>
 </div>
