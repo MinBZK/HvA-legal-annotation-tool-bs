55bf7cd66c3bee870105825784745b7663e3f4db
lamwf <wing.fung.lam@hva.nl>
2024-01-17 21:08:52 +0100

    feat: update DocumentStore

--- src/stores/DocumentStore.ts
+++ src/stores/DocumentStore.ts
@@ -1,7 +1,9 @@
-import { writable } from 'svelte/store';
+// import { writable } from 'svelte/store';
+import type {Writable} from 'svelte/store';
 import type LegalDocument from '../models/LegalDocument';
+import { localStorageStore } from '@skeletonlabs/skeleton';
 
-export const documentStore = writable<LegalDocument>({
+export const documentStore:Writable<LegalDocument> = localStorageStore('legal-document',{
     title: '',
     filename: '',
     chapterTitles: [],
